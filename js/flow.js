40e08e1daf25d63ad1abd547a32542bd8fe774b7
DeeGemini <zodiactheegemini@gmail.com>
2024-12-03 18:04:09 +0200

    Updated

--- js/flow.js
+++ js/flow.js
@@ -5,3 +5,11 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navList.classList.toggle('active');
 });
+
+const typed = new Typed('.multiple', {
+    strings: ['Software Engineer Graduate, Building Solutions in JS, Backend Dev, Blogger'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+});
